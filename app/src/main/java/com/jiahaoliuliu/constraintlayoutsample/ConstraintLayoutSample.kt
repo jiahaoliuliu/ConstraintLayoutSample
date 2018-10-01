493ac5670fc4a8202e7282c34da2a18e0582244d
Jiahao Liu <jiahao.liu@dnata.com>
2018-10-01 14:13:02 +0400

    Uploading the first dummy version

---
+++ app/src/main/java/com/jiahaoliuliu/constraintlayoutsample/ConstraintLayoutSample.kt
@@ -0,0 +1,12 @@
+package com.jiahaoliuliu.constraintlayoutsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintLayoutSample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout_sample)
+    }
+}
